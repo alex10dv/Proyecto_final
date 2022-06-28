5c91ec701205a53b93f5e87cd52df8c9665dbc29
alex10dv <99297169+alex10dv@users.noreply.github.com>
2022-06-28 16:15:18 -0300

    Actualizacion de progreso

--- proyecto_final/src/app/app.module.ts
+++ proyecto_final/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
